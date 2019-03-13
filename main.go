641a7e545df88ebad071cdf2f568b7faeb156109
Andrei Petrescu <andrei.petrescu@cloudhero.io>
2019-03-13 22:35:10 +0200

    finished first problem

--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"os"
+	"sort"
 	"strings"
 	"time"
 )
@@ -28,42 +29,48 @@ func timer(limit int, correctAnswers *int, noQuestions int) {
 func main() {
 	fileName := flag.String("filename", "problems.csv", "Filename for the quiz with questions and answers")
 	limit := flag.Int("limit", 30, "Time limit for the quiz")
+	shuffle := flag.Bool("shuffle", false, "pass true to shuffle the quiz, and false, to remain as in the file")
 
 	flag.Parse()
 
 	data, err := ioutil.ReadFile(*fileName)
 	inputReader := bufio.NewReader(os.Stdin)
 
-	noQuestions := 0
-
-	for i := 0; i < len(data); i++ {
-		if data[i] == 10 {
-			noQuestions++
-		}
-	}
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	r := csv.NewReader(strings.NewReader(string(data)))
+	questions := strings.Split(string(data), "\n")
+
+	if questions[len(questions)-1] == "" {
+		questions = questions[0 : len(questions)-1]
+	}
 
 	correctAnswers := 0
 
 	fmt.Print("Press enter to start the game!")
 	inputReader.ReadString('\n')
 
-	go timer(*limit, &correctAnswers, noQuestions)
+	if *shuffle {
+		rand.Seed(time.Now().UTC().UnixNano())
 
-	i := 0
+		sort.Slice(questions, func(i, j int) bool {
+			randNumber := rand.Int() % 2
+			randBool := false
 
-	for {
+			if randNumber == 1 {
+				randBool = true
+			}
+
+			return randBool
+		})
+	}
+
+	go timer(*limit, &correctAnswers, len(questions))
+
+	for i := 0; i < len(questions); i++ {
+		r := csv.NewReader(strings.NewReader(questions[i]))
 		record, err := r.Read()
-
-		if err == io.EOF {
-			fmt.Println("You scored", correctAnswers, "out of", noQuestions)
-			break
-		}
 
 		if err != nil {
 			log.Fatal(err)
@@ -76,11 +83,10 @@ func main() {
 			log.Fatal(err)
 		}
 
-		i++
-
-		if strings.TrimSpace(answer) == record[1] {
+		if strings.ToLower(strings.TrimSpace(answer)) == strings.ToLower(strings.TrimSpace(record[1])) {
 			correctAnswers++
 		}
 	}
 
+	fmt.Println("You scored", correctAnswers, "out of", len(questions))
 }
